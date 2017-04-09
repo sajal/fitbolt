d7dbd975f41f58481c3f4916d3d32b8c9a49cb1c
Sajal Kayan <sajal@turbobytes.com>
2017-04-09 23:06:08 +0700

    use a cli library for managing subcommands

--- README.md
+++ README.md
@@ -9,18 +9,36 @@ Syncs from Fitbit API and stores data into a local [Bolt](https://github.com/bol
 
 ```
 $ fitbolt --help
-Usage of fitbolt:
-  -creds string
-    	Path to store/load fitbit tokens from (default "/home/sajal/.fitsyncgo")
-  -db string
-    	path to bolt database(gets created if not exists) (default "/home/sajal/fitsyncgo.db")
-  -genisis string
-    	The date to start fetching data from (default "2016-01-01")
-exit status 2
+NAME:
+   fitbolt - sync and query fitbit data locally
+
+USAGE:
+   fitbolt [global options] command [command options] [arguments...]
+
+VERSION:
+   0.0.1
+
+COMMANDS:
+     sync     Sync with fitbit, needs FITBIT_CLIENT and FITBIT_SECRET to be set
+     steps    list steps by day
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --dbpath value   path to bolt database(gets created if not exists) (default: "/home/sajal/fitsyncgo.db")
+   --creds value    path to bolt database(gets created if not exists) (default: "/home/sajal/.fitsyncgo")
+   --genesis value  The date to start fetching data from (default: "2016-01-01")
+   --help, -h       show help
+   --version, -v    print the version
 ```
+
+### sync
 
 [Register](https://dev.fitbit.com/apps/new) an app with Fitbit, make sure it is of type [Personal](https://dev.fitbit.com/docs/basics/#personal)
 
-    FITBIT_CLIENT="<OAuth 2.0 Client ID>" FITBIT_SECRET="<Client Secret>" fitbolt
+    FITBIT_CLIENT="<OAuth 2.0 Client ID>" FITBIT_SECRET="<Client Secret>" fitbolt sync
 
-This should start fetching data from `genisis`. Due to [stringent rate-limits in Fitbit API](https://dev.fitbit.com/docs/basics/#rate-limits), you can only fetch ~30 days of data per hour. So to bootstrap the database, you would need to run fitbolt every hour until it catches up.
+This should start fetching data from `genesis`. Due to [stringent rate-limits in Fitbit API](https://dev.fitbit.com/docs/basics/#rate-limits), you can only fetch ~30 days of data per hour. So to bootstrap the database, you would need to run fitbolt every hour until it catches up.
+
+### Query sub commands
+
+TODO
--- cmd/fitbolt/main.go
+++ cmd/fitbolt/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"log"
 	"os"
 	"os/user"
 
+	cli "gopkg.in/urfave/cli.v1"
+
 	"github.com/sajal/fitbolt"
 )
 
-var (
-	db      *fitbolt.BoltDB
-	syncer  *fitbolt.Syncer
-	genisis *string
-)
+func sync(dbpath, fname, fclient, fsecret, genesis string) error {
+	if dbpath == "" {
+		return fmt.Errorf("dbpath is blank")
+	}
+	if fname == "" {
+		return fmt.Errorf("creds is blank")
+	}
+	if genesis == "" {
+		return fmt.Errorf("genesis is blank")
+	}
+	if fclient == "" {
+		return fmt.Errorf("FITBIT_CLIENT must be set")
+	}
+	if fsecret == "" {
+		return fmt.Errorf("FITBIT_SECRET must be set")
+	}
+	db, err := fitbolt.NewBoltDB(dbpath)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	syncer, err := fitbolt.NewSyncer(fname, fclient, fsecret, db)
+	if err != nil {
+		return err
+	}
+	return syncer.Sync(genesis)
+}
 
-func init() {
+func main() {
 	var err error
 	usr, err := user.Current()
 	if err != nil {
@@ -27,37 +51,55 @@ func init() {
 	fClient := os.Getenv("FITBIT_CLIENT")
 	fSecret := os.Getenv("FITBIT_SECRET")
 
-	//Provide flags with defaults
-	fitbitCred := flag.String("creds", fname, "Path to store/load fitbit tokens from")
-	dbPath := flag.String("db", dbpath, "path to bolt database(gets created if not exists)")
-	genisis = flag.String("genisis", "2016-01-01", "The date to start fetching data from")
-	flag.Parse()
-
-	//Check env variables
-	if fClient == "" {
-		log.Fatal("FITBIT_CLIENT must be set")
-	}
-	if fSecret == "" {
-		log.Fatal("FITBIT_SECRET must be set")
-	}
-
-	db, err = fitbolt.NewBoltDB(*dbPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	syncer, err = fitbolt.NewSyncer(*fitbitCred, fClient, fSecret, db)
-	if err != nil {
-		log.Fatal(err)
+	app := cli.NewApp()
+	app.Name = "fitbolt"
+	app.Usage = "sync and query fitbit data locally"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "dbpath",
+			Value: dbpath,
+			Usage: "path to bolt database(gets created if not exists)",
+		},
+		cli.StringFlag{
+			Name:  "creds",
+			Value: fname,
+			Usage: "path to bolt database(gets created if not exists)",
+		},
+		cli.StringFlag{
+			Name:  "genesis",
+			Value: "2016-01-01",
+			Usage: "The date to start fetching data from",
+		},
 	}
 
-}
-
-func main() {
-	defer db.Close()
-
-	err := syncer.Sync(*genisis)
-	if err != nil {
-		log.Fatal(err)
+	app.Commands = []cli.Command{
+		{
+			Name:  "sync",
+			Usage: "Sync with fitbit, needs FITBIT_CLIENT and FITBIT_SECRET to be set",
+			Action: func(c *cli.Context) error {
+				return sync(c.GlobalString("dbpath"), c.GlobalString("creds"), fClient, fSecret, c.GlobalString("genesis"))
+			},
+		},
+		{
+			Name:  "steps",
+			Usage: "list steps by day",
+			Action: func(c *cli.Context) error {
+				return fmt.Errorf("not implimented yet")
+			},
+		},
 	}
+
+	app.Run(os.Args)
+
+	/*
+		defer db.Close()
+		syncer, err := fitbolt.NewSyncer(*fitbitCred, fClient, fSecret, db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = syncer.Sync(*genisis)
+		if err != nil {
+			log.Fatal(err)
+		}*/
 }
